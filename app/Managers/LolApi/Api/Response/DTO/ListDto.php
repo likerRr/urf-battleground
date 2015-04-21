3f2c448e1a0b509dc8fc3594fac89309b1ebc819
Alexey Lizurchik <al.lizurchik@gmail.com>
2015-04-21 14:56:43 +0300

    wtf

--- app/Managers/LolApi/Api/Response/DTO/ListDto.php
+++ app/Managers/LolApi/Api/Response/DTO/ListDto.php
@@ -5,32 +5,18 @@ use URFBattleground\Managers\LolApi\Api\Response\ResponseDto;
 
 class ListDto extends ResponseDto {
 
-	/** @var \ArrayIterator */
 	private $list;
 
-	/**
-	 * @param Response $response
-	 */
 	public function __construct(Response $response)
 	{
 		parent::__construct($response);
-		$this->list = new \ArrayIterator((array) $this->response()->getData());
+		$this->list = new \ArrayIterator($this->response()->getData());
 	}
 
-	/**
-	 * Returns list of game ids or gets exactly game id by key in array
-	 * @param null $index
-	 * @param null $default
-	 * @return \ArrayIterator|mixed
-	 */
 	public function getList($index = null, $default = null)
 	{
 		if ($index !== null && is_int($index)) {
-			if (!$this->list->offsetExists($index)) {
-				return $default;
-			}
-
-			return $this->list->offsetGet($index);
+			return array_get($this->list->getArrayCopy(), $index, $default);
 		}
 
 		return $this->list;
---
+++ app/Managers/LolApi/Api/Response/DTO/ParticipantIdentityDto.php
@@ -0,0 +1,5 @@
+<?php namespace URFBattleground\Managers\LolApi\Api\Response\Dto;
+
+class ParticipantIdentityDto {
+
+}

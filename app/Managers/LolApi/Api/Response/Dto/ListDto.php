86483ddc7687a357c845994634c333ec9b9ce392
Alexey Lizurchik <al.lizurchik@gmail.com>
2015-04-22 01:23:58 +0300

    return Dto back

---
+++ app/Managers/LolApi/Api/Response/Dto/ListDto.php
@@ -0,0 +1,40 @@
+<?php namespace URFBattleground\Managers\LolApi\Api\Response\Dto;
+
+use URFBattleground\Managers\LolApi\Api\Response\Response;
+use URFBattleground\Managers\LolApi\Api\Response\ResponseDto;
+use URFBattleground\Managers\LolApi\Engine\Support\Collection;
+
+class ListDto extends ResponseDto {
+
+	/** @var \ArrayIterator */
+	private $list;
+
+	/**
+	 * @param Response $response
+	 */
+	public function __construct(Response $response)
+	{
+		parent::__construct($response);
+		$this->list = new Collection((array) $this->response()->getData());
+	}
+
+	/**
+	 * Returns list of game ids or gets exactly game id by key in array
+	 * @param null $index
+	 * @param null $default
+	 * @return \ArrayIterator|mixed
+	 */
+	public function getList($index = null, $default = null)
+	{
+		if ($index !== null && is_int($index)) {
+			if (!$this->list->offsetExists($index)) {
+				return $default;
+			}
+
+			return $this->list->offsetGet($index);
+		}
+
+		return $this->list;
+	}
+
+}
